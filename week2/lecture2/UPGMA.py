0660bb8205a6947f541a63d83faf2870a05b880f
yeojingi <businessjingi@gmail.com>
2023-01-09 23:02:37 +0900

    week2

--- week2/lecture2/UPGMA.py
+++ week2/lecture2/UPGMA.py
@@ -8,9 +8,13 @@ def find_closest_clusters(D):
 
     for i in range(len(D)):
         for j in range(len(D[0])):
+            if i == j:
+                continue
+
             if D[i][j] < d:
                 mi = i
                 mj = j
+                d = D[i][j]
     
     return [mi, mj, d]
 
@@ -22,6 +26,8 @@ def upgma(D, n):
 
     while len(clusters) > 1:
         [mi, mj, md] = find_closest_clusters(D_cluster)
+        print(clusters)
+        print(*D_cluster, sep="\n")
         I = clusters[mi]
         J = clusters[mj]
         # merge Ci, Cj into C_new
@@ -30,11 +36,14 @@ def upgma(D, n):
         # add a new node
         T.append(len(T))
         # remove cluster, T
-        a, b = mj, mi if mi > mj else mi, mj
+        [a, b] = [mj, mi] if mi > mj else [mi, mj]
+
+        print(a, b, md)
+
         clusters.pop(b)
         clusters.pop(a)
-        T.pop(b)
-        T.pop(a)
+        # T.pop(b)
+        # T.pop(a)
         D_cluster.pop(b)
         D_cluster.pop(a)
         for d in D_cluster:
@@ -42,18 +51,35 @@ def upgma(D, n):
             d.pop(a)
         
         # connect C_new to Ci and Cj
-        for k in range(len(D_cluster) - 1):
+        for k in range(len(D_cluster)):
             # connect C_new and Ck
             D_k_new = 0
 
             Ck = clusters[k]
             for m in Ck:
                 for i in I:
-                    D_k_new += D[i][m] * len(I)
+                    D_k_new += D[i][m] * len(I) / len(Ck)
 
                 for j in J:
-                    D_k_new += D[j][m] * len(J)
+                    D_k_new += D[j][m] * len(J) / len(Ck)
                 
+            print(D_k_new, 'knew', len(I) + len(J))
             D_k_new /= (len(I) + len(J))
+
+            D_cluster[k].append(D_k_new)
         
-        # Age[mi] = 
+        # for k in range(len(D_cluster)):
+        #     d.append(D_k_new)
+        D_cluster.append([ D_cluster[k][-1] for k in range(len(D_cluster))] + [0])
+
+        Age[mi] = D_k_new / 2
+    
+    print(Age)
+    return T
+
+fn = "1.txt"
+f = open(f"./data/{fn}", "r")
+N = int(f.readline().strip())
+matrix = [ list(map(int, f.readline().strip().split())) for _ in range(N) ]
+res = upgma(matrix, N)
+print(res)
